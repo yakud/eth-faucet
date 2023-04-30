46a811cabe28cc600b6c8917be6e0ac3d3cbb2b4
yakud <yakudgm@gmail.com>
2023-04-30 03:05:28 +0200

    Add metamask address button.

--- web/src/Faucet.svelte
+++ web/src/Faucet.svelte
@@ -3,6 +3,7 @@
   import { getAddress } from '@ethersproject/address';
   import { CloudflareProvider } from '@ethersproject/providers';
   import { setDefaults as setToast, toast } from 'bulma-toast';
+  import detectEthereumProvider from '@metamask/detect-provider';
 
   let input = null;
   let faucetInfo = {
@@ -78,6 +79,50 @@
     const lower = str.toLowerCase();
     return str.charAt(0).toUpperCase() + lower.slice(1);
   }
+
+  let ethProvider;
+  let accountAddress = '';
+
+  onMount(async () => {
+    ethProvider = await detectEthereumProvider();
+    ethProvider.addListener('accountsChanged', (accounts) => {
+      if (accounts.length === 0) {
+        window.location.reload();
+        accountAddress = '';
+        return;
+      }
+      accountAddress = accounts[0];
+    });
+
+    ethProvider.addListener('chainChanged', (chainId) => {
+      window.location.reload();
+      accountAddress = '';
+    });
+
+    ethProvider.addListener('disconnect', (error) => {
+      window.location.reload();
+      accountAddress = '';
+    });
+
+    ethProvider.addListener('networkChanged', (networkId) => {
+      window.location.reload();
+      accountAddress = '';
+    });
+  });
+
+  async function connectMetaMask() {
+    if (!ethProvider) {
+      alert('MetaMask is not installed');
+      return;
+    }
+
+    try {
+      const accounts = await ethProvider.request({ method: 'eth_requestAccounts' });
+      accountAddress = accounts[0];
+    } catch (error) {
+      console.error('Error connecting to MetaMask:', error);
+    }
+  }
 </script>
 
 <main>
@@ -123,14 +168,20 @@
           </h2>
           <div class="box">
             <div class="field is-grouped">
-              <p class="control is-expanded">
+              <p class="control is-expanded has-icons-right">
                 <input
-                  bind:value={input}
+                  value={accountAddress}
                   class="input is-rounded"
                   type="text"
                   placeholder="Enter your address or ENS name"
                 />
+                <button class="icon-button" type="button" on:click={connectMetaMask}>
+                  <span class="icon is-small is-clickable">
+                    <img src="./metamask2.png" style="width: 20px; height: 20px;" />
+                  </span>
+                </button>
               </p>
+
               <p class="control">
                 <button
                   on:click={handleRequest}
@@ -162,4 +213,25 @@
   .box {
     border-radius: 19px;
   }
+  .icon-button {
+    background: none;
+    border: none;
+    padding: 0;
+    position: absolute;
+    right: 45px;
+    top: 15%;
+    transform: translateY(-50%);
+    cursor: pointer;
+  }
+  .is-clickable {
+    cursor: pointer;
+  }
+
+  .input.is-rounded + .is-clickable {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
 </style>
